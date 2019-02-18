45ad39d629ed300d16ed23607061da1bf5d478bb
Elise Phan <aliceconnic@gmail.com>
2019-02-18 22:38:55 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+    get 'authors/:id/posts', to: 'authors#posts_index'
+  get 'authors/:id/posts/:post_id', to: 'authors#post'
+  
   resources :artists
   resources :songs
+    get 'artist/:id/songs', to: 'artists#songs_index'
+    get 'artists/:id/songs/:song_id', to: 'artists#song'
 end
