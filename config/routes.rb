fe7ff5f7e5b3b296b4fbbfd56e9a7ee827c74cf0
Elise Phan <aliceconnic@gmail.com>
2019-02-18 23:55:44 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  
-    get 'authors/:id/posts', to: 'authors#posts_index'
-  get 'authors/:id/posts/:post_id', to: 'authors#post'
-  
-  resources :artists
-  resources :songs
-    get 'artist/:id/songs', to: 'artists#songs_index'
-    get 'artists/:id/songs/:song_id', to: 'artists#song'
+  resources :artists, only: [:index,:show] do
+    resources :songs, only: [:show, :index]
+  end
+ 
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
+ 
+  root 'songs#index'
 end
